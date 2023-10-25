b6ebfec545194a62bc05d5cd004dfc4fd1e18d77
neyser <neyser@digitechfp.com>
2023-10-25 14:05:58 +0200

    Cambios

---
+++ js/motor.js
@@ -0,0 +1,17 @@
+const b1 = document.querySelector("#b1");
+
+if (b1){
+    // Referencia de los elementos
+    const response1 = document.querySelector("#resultado1");
+    const urlJSON1 = 'json/bd1.json';
+    const method1 = 'GET';
+    const searchData1 = document.querySelector("#c1");
+
+    b1.addEventListener("click", function(){
+        alert("Hola");
+    });
+}
+
+
+// Uso de defer
+// Uso de queryselector
